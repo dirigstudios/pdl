73ec1d34a05fe1f4358d291929ef41595e50bf6c
gabri <70864426+fkyros@users.noreply.github.com>
2022-10-13 20:58:43 +0200

    update gitignore

---
+++ src/AFD.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class AFD 
+{
+    public static void getToken(Scanner fichero, )
+    {
+        String line;
+        char c;
+        int i;
+
+        i = 0;
+        while(fichero.hasNextLine())
+        {
+            line = fichero.nextLine();
+            c = line.charAt(i);
+        }
+    }
+
+    public static void main(String[] args) 
+    {
+        Scanner fichero = new Scanner("../tests/ejemplo");
+        getToken(fichero);
+        fichero.close();
+    }
+}
