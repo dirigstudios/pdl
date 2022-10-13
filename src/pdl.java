9354676bd4404e2a2679ea1f4678eef2939bc34b
Vasapg <victoralejandro.sanz@alumnos.upm.es>
2022-10-13 19:44:52 +0200

    Saving changes on the TPR table

--- src/pdl.java
+++ src/pdl.java
@@ -1,17 +1,52 @@
 import java.io.FileReader;
 import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class pdl {
 
+    public static final ArrayList<String> TPR = new ArrayList<>();
     public static boolean isDel(char c)
     {
         if (c == '\n' || c == '\t' || c == ' ' || c == ';')
             return true;
         return false;
     }
+    public static boolean isD(char c)
+    {
+        if ((int)c <= 48 && (int)c >= 57)
+            return true;
+        return false;
+    }
+
+    public static boolean isC(char c)
+    {
+        if ((int)c <= 97 && (int)c >= 122)
+            return true;
+        return false;
+    }
+
+    public static void initializeTPR()
+    {
+        TPR.add("boolean");
+        TPR.add("boolean");
+        TPR.add("boolean");
+        TPR.add("boolean");
+        TPR.add("boolean");
+        TPR.add("boolean");
+        TPR.add("boolean");
+        TPR.add("boolean");
+        TPR.add("boolean");
+        TPR.add("boolean");
+        TPR.add("boolean");
+        TPR.add("boolean");
+        TPR.add("boolean");
+        TPR.add("boolean");
+
+    }
 
     public static void main(String[] args) throws FileNotFoundException {
-        FileReader sc = new FileReader(args[0]);
+        FileReader sc = new FileReader("C:\\Proyectos\\pdl\\pdl\\src\\texto.txt");
         String fin = "";
         int valor = 0;
         while (valor != -1 && !isDel((char)valor)) {
