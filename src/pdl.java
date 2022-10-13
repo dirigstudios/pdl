d4839ccd9463fd9a724c89336de53605fb6a5d4c
Vasapg <victoralejandro.sanz@alumnos.upm.es>
2022-10-13 20:53:15 +0200

    Uploading changes on Automata Function

--- src/pdl.java
+++ src/pdl.java
@@ -1,16 +1,19 @@
 import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.lang.Character;
 
 public class pdl {
 
     public static final ArrayList<String> TPR = new ArrayList<>();
+
     public static boolean isDel(char c)
     {
         if (c == '\n' || c == '\t' || c == ' ' || c == ';')
             return true;
         return false;
     }
+
     public static boolean isD(char c)
     {
         if ((int)c <= 48 && (int)c >= 57)
@@ -28,23 +31,63 @@ public class pdl {
     public static void initializeTPR()
     {
         TPR.add("boolean");
-        TPR.add("boolean");
-        TPR.add("boolean");
-        TPR.add("boolean");
-        TPR.add("boolean");
-        TPR.add("boolean");
-        TPR.add("boolean");
-        TPR.add("boolean");
-        TPR.add("boolean");
-        TPR.add("boolean");
-        TPR.add("boolean");
-        TPR.add("boolean");
-        TPR.add("boolean");
-        TPR.add("boolean");
+        TPR.add("break");
+        TPR.add("case");
+        TPR.add("function");
+        TPR.add("if");
+        TPR.add("input");
+        TPR.add("int");
+        TPR.add("let");
+        TPR.add("print");
+        TPR.add("return");
+        TPR.add("string");
+        TPR.add("switch");
+    }
 
+    public static int next_stage_S(char c)
+    {
+        if (isC(c))
+            return 2;
+        if (c == '\"')
+            return 3;
+        if (isD(c))
+            return 4;
+        if (c == '%')
+            return 5;
+        if (c == '=')
+            return 6;
+        if (c == '{')
+            return 7;
+        if (c == '}')
+            return 8;
+        return -1;
+    }
+
+    public static void Automata(int stage)
+    {
+        char c = 'a'; //Delete intialize
+        switch (stage)
+        {
+            case 1:             // Estado S
+            {
+                //c = read_next_char(fd);
+                Automata(next_stage_S(c));
+                break;
+            }
+            case 2:             // Estado T
+            {
+
+            }
+            case 3:             // Estado A
+            {
+
+            }
+            case 4:             // Estado B
+        }
     }
     
     public static void main(String[] args) throws IOException {
+        initializeTPR();
         FileReader sc = new FileReader(args[0]);
         String fin = "";
         int valor = 0;
