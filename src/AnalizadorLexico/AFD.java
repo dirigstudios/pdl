c6ab57f937baa1a582cc0c128ada0a2b41d46273
Vasapg <victoralejandro.sanz@alumnos.upm.es>
2022-10-18 13:13:23 +0200

    add final stage to support line recieve

--- src/AnalizadorLexico/AFD.java
+++ src/AnalizadorLexico/AFD.java
@@ -3,14 +3,17 @@ package AnalizadorLexico;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 
-public class AFD 
-{
-    public enum Estados{Inicial, AsignacionR, ConstanteNumerica, Cadena, PalabraReservada, Comparacion, Asignacion,
-                        AbrePar, CierraPar, AbreLlave, CierraLlave, PuntoComa, DosPuntos, Coma, Negacion, Suma}
+public class AFD {
+    public enum Estados {
+        Inicial, AsignacionR, ConstanteNumerica, Cadena, PalabraReservada, Comparacion, Asignacion,
+        AbrePar, CierraPar, AbreLlave, CierraLlave, PuntoComa, DosPuntos, Coma, Negacion, Suma, Final
+    }
+
     public static Estados estadoactual = Estados.Inicial;
 
-    public static boolean leerSigCaracter = true;
     public static PrintWriter fd;
+
+
     public static Estados nextStage(char c) //TODO ADD IMPLEMENTATION FOR UPPERCASE CHARACTERS and variables that start with _
     {
         if (Character.isLowerCase(c))
@@ -44,46 +47,38 @@ public class AFD
         return Estados.Inicial;
     }
 
-    public static void automata(String palabra, PrintWriter fd)
-    {
+    public static int automata(String palabra, PrintWriter fd) {
         int i = 0;
         char c = 0;
         String lex = "";
         int counter = 0;
-        while(i < palabra.length())        //TODO pensar en como tratar el ultimo caracter de la linea
+        boolean fin = false;
+        while (i < palabra.length() && !fin)        //TODO pensar en como tratar el ultimo caracter de la linea
         {
             c = palabra.charAt(i);
             if (!(estadoactual == Estados.Inicial))
                 i++;
-            switch (estadoactual)
-            {
+            switch (estadoactual) {
                 case Inicial: // Estado S
                     estadoactual = nextStage(c);
-                    if (!Token.isDel(c))
-                        leerSigCaracter = false;
                     break;
-
                 case PalabraReservada: // Estado T
                     lex += String.valueOf(c);
                     if (Token.TPR.contains(lex)) //TODO pensar como implementar la condicion de que el sig caracter sea del, sin salir del index del la linea
                     {
                         Token.genToken("palabraReservada", lex, fd);
                         lex = "";
-                        estadoactual = Estados.Inicial;
-                        leerSigCaracter = false;
+                        estadoactual = Estados.Final;
                     }
                     break;
 
-                case Cadena: // Estado A
-                    if (((int) c == 0 || c == '"') && counter <= 64 && lex != "")
-                    {
+                case Cadena:
+                    if (((int) c == 0 || c == '"') && counter <= 64 && lex != "") {
                         Token.genToken("cadena", lex, fd);
                         lex = "";
                         counter = 0;
-                        estadoactual = Estados.Inicial;
-                    }
-                    else
-                    {
+                        estadoactual = Estados.Final;
+                    } else {
                         if (c != '"')
                             lex += (String.valueOf(c));
                         counter++;
@@ -91,103 +86,100 @@ public class AFD
                     }
                     break;
 
-                case ConstanteNumerica:             // Estado B
+                case ConstanteNumerica:
                     if (Character.isDigit(c) && i < palabra.length() && Character.isDigit(palabra.charAt(i)))
                         counter = (Character.getNumericValue(c) + counter) * 10;
-                    else if (counter > 32767)
-                    {
+                    else if (counter > 32767) {
                         System.out.println("Error, valor numerico excede los limites");
-                    }
-                    else
+                    } else
                         counter = (Character.getNumericValue(c) + counter);
-                    if (!Character.isDigit(c) || i == palabra.length() && counter <= 32767)
-                    {
+                    if (!Character.isDigit(c) || i == palabra.length() && counter <= 32767) {
                         Token.genToken("constEnt", Integer.toString(counter), fd);
                         counter = 0;
-                        estadoactual = Estados.Inicial;
-                        leerSigCaracter = false;
+                            estadoactual = Estados.Final;
                     }
                     break;
 
                 case AsignacionR:
-                    if(palabra.charAt(i) == '=')
-                    {
+                    if (palabra.charAt(i) == '=') {
                         Token.genToken("asignacionResto", "-", fd);
-                        estadoactual = Estados.Inicial;
+                        estadoactual = Estados.Final;
                         i++;
-                    }
-                    else
+                    } else
                         System.out.println("Error: simbolo no reconocido");
                     break;
-                
+
                 case Asignacion:
-                    if (palabra.charAt(i) != '=')
-                    {
+                    if (palabra.charAt(i) != '=') {
                         Token.genToken("igual", "-", fd);
-                        estadoactual = Estados.Inicial;
-                    }
-                    else
+                        estadoactual = Estados.Final;
+                    } else
                         estadoactual = Estados.Comparacion;
                     break;
 
                 case Comparacion:
                     Token.genToken("comparacion", "-", fd);
-                    estadoactual = Estados.Inicial;
+                    estadoactual = Estados.Final;
                     break;
-                
+
                 case AbreLlave:
                     Token.genToken("abreLlave", "-", fd);
-                    estadoactual = Estados.Inicial;
+                    estadoactual = Estados.Final;
                     break;
 
                 case CierraLlave:
                     Token.genToken("cierraLlave", "-", fd);
-                    estadoactual = Estados.Inicial;
+                    estadoactual = Estados.Final;
                     break;
-                
+
                 case AbrePar:
                     Token.genToken("abrePar", "-", fd);
-                    estadoactual = Estados.Inicial;
+                    estadoactual = Estados.Final;
                     break;
-                
+
                 case CierraPar:
                     Token.genToken("cierraPar", "-", fd);
-                    estadoactual = Estados.Inicial;
+                    estadoactual = Estados.Final;
                     break;
-                
+
                 case DosPuntos:
                     Token.genToken("dosPuntos", "-", fd);
-                    estadoactual = Estados.Inicial;
+                    estadoactual = Estados.Final;
                     break;
-                
+
                 case PuntoComa:
                     Token.genToken("puntoComa", "-", fd);
-                    estadoactual = Estados.Inicial;
+                    estadoactual = Estados.Final;
                     break;
 
                 case Coma:
                     Token.genToken("coma", "-", fd);
-                    estadoactual = Estados.Inicial;
+                    estadoactual = Estados.Final;
                     break;
 
                 case Negacion:
                     Token.genToken("negacion", "-", fd);
-                    estadoactual = Estados.Inicial;
+                    estadoactual = Estados.Final;
                     break;
 
                 case Suma:
                     Token.genToken("suma", "-", fd);
-                    estadoactual = Estados.Inicial;
-                   break;
+                    estadoactual = Estados.Final;
+                    break;
+
+                case Final:
+                    fin = true;
+                    break;
             }
         }
+        return i - 1;
     }
-
+/*
     public static void main(String[] args) throws FileNotFoundException {
         Token.initializeTPR();
         fd = new PrintWriter("./tests/tokens.txt");
-        automata("ifputamadre",fd);
+        System.out.println(automata("12333", fd));
         fd.close();
     }
-
+*/
 }
